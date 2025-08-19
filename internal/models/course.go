eb2fa2257d5eea06125d664e940eb2405e8f1914
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-08-19 20:36:24 +0700

    feat: change topics data type in Course model

--- internal/models/course.go
+++ internal/models/course.go
@@ -1,15 +1,16 @@
 package models
 
 import (
+	"github.com/lib/pq"
 	"gorm.io/gorm"
 )
 
 type Course struct {
 	gorm.Model
-	Title          string   `json:"title" gorm:"size:200;not null"`
-	Description    string   `json:"description" gorm:"type:text;not null"`
-	Instructor     string   `json:"instructor" gorm:"size:100;not null"`
-	Topics         []string `json:"topics" gorm:"type:text[];not null;default:'{}'"`
-	Price          float64  `json:"price" gorm:"type:numeric(10,2);not null"`
-	ThumbnailImage string   `json:"thumbnail_image" gorm:"size:255"`
+	Title          string         `json:"title" gorm:"size:200;not null"`
+	Description    string         `json:"description" gorm:"type:text;not null"`
+	Instructor     string         `json:"instructor" gorm:"size:100;not null"`
+	Topics         pq.StringArray `json:"topics" gorm:"type:text[];not null;default:'{}'"`
+	Price          float64        `json:"price" gorm:"type:numeric(10,2);not null"`
+	ThumbnailImage string         `json:"thumbnail_image" gorm:"size:255"`
 }
