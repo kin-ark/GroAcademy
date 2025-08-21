1218a59853f7fb2cd23be8f8aa1f63489c9c4ac7
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-08-22 02:46:38 +0700

    refactor: refactor routes.go structure

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,64 +9,77 @@ import (
 )
 
 func RegisterRoutes(r *gin.Engine) {
-	// FOR TESTING PURPOSE ONLY, WILL BE DELETED LATER
+	// Dependency Injection
 	userRepo := repositories.NewUserRepository()
-	authService := services.NewAuthService(userRepo)
-	authController := controllers.NewAuthController(authService)
-	userService := services.NewUserService(userRepo)
-	userController := controllers.NewUserController(userService)
-
 	courseRepo := repositories.NewCourseRepository()
-	courseService := services.NewCourseService(courseRepo)
-	courseController := controllers.NewCourseController(courseService)
-
 	moduleRepo := repositories.NewModuleRepository()
+
+	authService := services.NewAuthService(userRepo)
+	userService := services.NewUserService(userRepo)
+	courseService := services.NewCourseService(courseRepo)
 	moduleService := services.NewModuleService(moduleRepo, courseRepo)
+
+	authController := controllers.NewAuthController(authService)
+	userController := controllers.NewUserController(userService)
+	courseController := controllers.NewCourseController(courseService)
 	moduleController := controllers.NewModuleController(moduleService)
 
 	api := r.Group("/api")
 	{
-		// Auth Route
-		auth := api.Group("/auth")
-		{
-			auth.POST("/login", authController.Login)
-			auth.POST("/register", authController.Register)
-			auth.GET("/self", middlewares.RequireAuth, authController.GetSelf)
-		}
+		registerAuthRoutes(api, &authController)
+		registerCourseRoutes(api, &courseController, &moduleController)
+		registerModuleRoutes(api, &moduleController)
+		registerUserRoutes(api, &userController)
+	}
+}
 
-		// CRUD Course Route
-		courses := api.Group("/courses")
-		courses.Use(middlewares.RequireAuth)
-		{
-			courses.POST("", middlewares.RequireAdmin, courseController.PostCourse)
-			courses.GET("", courseController.GetAllCourses)
-			courses.GET("/:id", courseController.GetCourseByID)
-			courses.PUT("/:id", middlewares.RequireAdmin, courseController.PutCourse)
-			courses.DELETE("/:id", middlewares.RequireAdmin, courseController.DeleteCourseByID)
-			courses.POST("/:id/buy", courseController.BuyCourse)
-			courses.GET("/my-courses", courseController.GetMyCourses)
+func registerAuthRoutes(api *gin.RouterGroup, authController *controllers.AuthController) {
+	auth := api.Group("/auth")
+	{
+		auth.POST("/login", authController.Login)
+		auth.POST("/register", authController.Register)
+		auth.GET("/self", middlewares.RequireAuth, authController.GetSelf)
+	}
+}
 
-			// CRUD Module
-			courses.POST("/:id/modules", middlewares.RequireAdmin, moduleController.PostModule)
-			courses.GET("/:id/modules", moduleController.GetModules)
-			courses.PATCH("/:id/modules/reorder", middlewares.RequireAdmin, moduleController.ReorderModules)
-			courses.PATCH("/:id/modules/complete", moduleController.MarkModuleAsComplete)
-		}
+func registerCourseRoutes(api *gin.RouterGroup, courseController *controllers.CourseController, moduleController *controllers.ModuleController) {
+	courses := api.Group("/courses")
+	courses.Use(middlewares.RequireAuth)
+	{
+		courses.POST("", middlewares.RequireAdmin, courseController.PostCourse)
+		courses.GET("", courseController.GetAllCourses)
+		courses.GET("/:id", courseController.GetCourseByID)
+		courses.PUT("/:id", middlewares.RequireAdmin, courseController.PutCourse)
+		courses.DELETE("/:id", middlewares.RequireAdmin, courseController.DeleteCourseByID)
 
-		// CRUD Module Route
-		modules := api.Group("/modules")
-		modules.Use(middlewares.RequireAuth)
-		{
-			modules.GET("/:id", moduleController.GetModuleById)
-			modules.PUT("/:id", middlewares.RequireAdmin, moduleController.PutModule)
-			modules.DELETE("/:id", middlewares.RequireAdmin, moduleController.DeleteModuleByID)
-		}
+		courses.POST("/:id/buy", courseController.BuyCourse)
+		courses.GET("/my-courses", courseController.GetMyCourses)
 
-		// CRUD User Route
-		api.GET("/users", userController.GetUsers)
-		api.GET("/users/:id", userController.GetUserById)
-		api.POST("/users/:id/balance", userController.AddUserBalance)
-		api.PUT("/users/:id", userController.PutUser)
-		api.DELETE("/users/:id", userController.DeleteUserByID)
+		courses.POST("/:id/modules", middlewares.RequireAdmin, moduleController.PostModule)
+		courses.GET("/:id/modules", moduleController.GetModules)
+		courses.PATCH("/:id/modules/reorder", middlewares.RequireAdmin, moduleController.ReorderModules)
+	}
+}
+
+func registerModuleRoutes(api *gin.RouterGroup, moduleController *controllers.ModuleController) {
+	modules := api.Group("/modules")
+	modules.Use(middlewares.RequireAuth)
+	{
+		modules.GET("/:id", moduleController.GetModuleById)
+		modules.PUT("/:id", middlewares.RequireAdmin, moduleController.PutModule)
+		modules.DELETE("/:id", middlewares.RequireAdmin, moduleController.DeleteModuleByID)
+		modules.PATCH("/:id/complete", moduleController.MarkModuleAsComplete)
+	}
+}
+
+func registerUserRoutes(api *gin.RouterGroup, userController *controllers.UserController) {
+	users := api.Group("/users")
+	users.Use(middlewares.RequireAuth, middlewares.RequireAdmin)
+	{
+		users.GET("", userController.GetUsers)
+		users.GET("/:id", userController.GetUserById)
+		users.POST("/:id/balance", userController.AddUserBalance)
+		users.PUT("/:id", userController.PutUser)
+		users.DELETE("/:id", userController.DeleteUserByID)
 	}
 }
