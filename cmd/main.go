8f59bab1ff7b5c0304e3175d73916744ab48dea6
Muhammad Kinan Arkansyaddad <mkinanarkansyaddad@gmail.com>
2025-08-24 05:16:59 +0700

    feat: add CORS

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"os"
+	"strings"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/kin-ark/GroAcademy/internal/database"
 	"github.com/kin-ark/GroAcademy/internal/routes"
@@ -10,6 +12,44 @@ import (
 
 func main() {
 	router := gin.Default()
+
+	config := cors.DefaultConfig()
+
+	allowedOrigins := os.Getenv("ALLOWED_ORIGINS")
+	if allowedOrigins != "" {
+		config.AllowOrigins = strings.Split(allowedOrigins, ",")
+	} else {
+		config.AllowOrigins = []string{
+			"http://localhost:3000",
+			"http://localhost:5173",
+			"http://127.0.0.1:3000",
+			"http://127.0.0.1:5173",
+		}
+	}
+
+	config.AllowMethods = []string{
+		"GET",
+		"POST",
+		"PUT",
+		"DELETE",
+		"OPTIONS",
+		"PATCH",
+	}
+
+	config.AllowHeaders = []string{
+		"Origin",
+		"Content-Type",
+		"Accept",
+		"Authorization",
+		"X-Requested-With",
+		"X-CSRF-Token",
+	}
+
+	config.AllowCredentials = true
+
+	config.MaxAge = 12 * 60 * 60
+
+	router.Use(cors.New(config))
 
 	router.Static("/static", "./static")
 	router.Static("/uploads", "./uploads")
